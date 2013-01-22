39b0903a0848720760bad06e18c66db4c9c549e8
Luke D'Alfonso <pokemonmegaman@yahoo.com>
2013-01-21 17:35:19 -1000

    Created base robot code

--- 2013FinalCode/MyRobot.cpp
+++ 2013FinalCode/MyRobot.cpp
@@ -5,61 +5,142 @@
  * The SimpleRobot class is the base of a robot application that will automatically call your
  * Autonomous and OperatorControl methods at the right time as controlled by the switches on
  * the driver station or the field controls.
- */ 
-class RobotDemo : public SimpleRobot
-{
+ */
+class RobotDemo : public SimpleRobot {
 	Joystick leftStick, rightStick;
-	Jaguar driveLeft, driveRight, endGameLeftFront, endGameRightFront, endGameLeftBack, endGameRightBack, shooterFront, shooterBack;
+	Jaguar driveLeft, driveRight, endGameLeftFront, endGameRightFront,
+			endGameLeftBack, endGameRightBack, shooterFront, shooterBack;
 	RobotDrive drive;
 	Relay light;
 	Servo pusher1, pusher2, feederLock;
-	
+	AnalogChannel potFront, potBack;
+
 public:
-	RobotDemo(void):
-		//Joysticks
-		leftStick(1),
-		rightStick(2),
-		//Jaguars
-		driveLeft(1),
-		driveRight(2),
-		endGameLeftFront(3), endGameRightFront(4), 
-		endGameLeftBack(5), endGameRightBack(6),
-		shooterFront(7),
-		shooterBack(8),
-		//Servos
-		pusher1(2,1), pusher2(2,2),
-		feederLock (2,3),
-		//Light
-		light(2,4),
-		//Drive
-		drive(1,2)
-	{
-		
+	RobotDemo(void) :
+				//Joysticks
+				leftStick(1),
+				rightStick(2),
+				//Jaguars
+				driveLeft(1), driveRight(2), endGameLeftFront(3),
+				endGameRightFront(4), endGameLeftBack(5), endGameRightBack(6),
+				shooterFront(7), shooterBack(8),
+				//Drive
+				drive(1, 2),
+				//Light
+				light(2, 4, Relay::kForwardOnly),
+				//Servos
+				pusher1(2, 1), pusher2(2, 2), feederLock(2, 3),
+				//Potentiometers
+				potFront(1, 1), potBack(1, 2) {
+
 	}
 
 	/**
 	 * Drive left & right motors for 2 seconds then stop
 	 */
-	void Autonomous(void)
-	{
+	void Autonomous(void) {
 		//TODO:Autnomous code
+		light.Set(Relay::kOn);
+		bool isAligned;
+		//Pyramid allignment
+		while (!isAligned) {
+			float targety = 0;
+			float targetx = 0;
+			float distance = 0;
+			//TODO: Get x and y values from NIVision or RoboRealm
+
+			//This code assumes that center of target is located at 100.
+			//TODO: Get actual middle values and replace 100
+
+			if (targetx < 98) { //+-2 error margin
+				//TODO: Math for the allignment algorithm
+			} else if (targetx > 102) {
+				//TODO: Math for the allignment algorithm
+			} else {
+				isAligned = true;
+			}
+
+			for (int x = 0; x<3; x++) {  //This code repeats 3 times to shoot all three disks
+				shooterBack.Set(1);
+				shooterFront.Set(1);
+				Wait(1.5); //Gives jags a chance to spin up  (TODO: find actual spin up time)
+				pusher1.Set(1);
+				pusher2.Set(1);
+				Wait(1); //Shoots the disk
+				shooterBack.Set(0);
+				shooterFront.Set(0);
+				pusher1.Set(0);
+				pusher2.Set(0);
+				Wait(1);
+				feederLock.Set(1);
+				Wait(1);
+				feederLock.Set(0);
+			}
+		}
+
 	}
 
 	/**
 	 * Runs the motors with arcade steering. 
 	 */
-	void OperatorControl(void)
-	{
-		
-		while (IsOperatorControl())
-		{
+	void OperatorControl(void) {
+		DriverStationLCD *screen = DriverStationLCD::GetInstance();
+		while (IsOperatorControl()) {
+			light.Set(Relay::kOn);
 			drive.TankDrive(leftStick, rightStick);//tank drive for now, but we can change to fit drivers needs.
-			//TODO:Shooter code - motor speeds and motor locks
-			
-			//TODO:End game code
+			if (rightStick.GetRawButton(1)) {
+				shooterBack.Set(1);
+				shooterFront.Set(1);
+				Wait(1.5); //Gives jags a chance to spin up
+				pusher1.Set(1);
+				pusher2.Set(1);
+				Wait(1); //Shoots the disk
+				pusher1.Set(0);
+				pusher2.Set(0);
+				shooterBack.Set(0);
+				shooterFront.Set(0);
+				Wait(1);
+				feederLock.Set(1);
+				Wait(1);
+				feederLock.Set(0);
+				//Do we want to put this in a for loop and fire all 4 disks, or shoot individual disks?
+			}
+			if (rightStick.GetRawButton(10)) { //End game code
+				bool isAligned;
+				//Pyramid allignment
+				while (!isAligned) {
+					float targety = 0;
+					float targetx = 0;
+					float distance = 0;
+					//TODO: Get x and y values from NIVision or RoboRealm
+					//This code assumes that center of target is located at 100. (TODO: Get actual middle values and replace 100)
+
+					if (targetx < 98) { //+-2 error margin
+						//TODO: Math for the allignment algorithm
+					} else if (targetx > 102) {
+						//TODO: Math for the allignment algorithm
+					} else {
+						isAligned = true;
+					}
+				}
+				while (potFront.GetVoltage() < (10/3)) { // 10/3 comes from 5(voltage)/270(degrees total)*180 degrees we need to move
+					endGameLeftFront.Set(1);
+					endGameRightFront.Set(1);
+				}
+				endGameLeftFront.Set(0);
+				endGameRightFront.Set(1);
+				while (potBack.GetVoltage() < (10/3)) {
+					endGameLeftBack.Set(1);
+					endGameRightBack.Set(1);
+				}
+				endGameLeftBack.Set(0);
+				endGameRightBack.Set(0);
+				//TODO: Repeat this code for the number of times we are going to move the arms
+			}
 		}
 	}
 };
 
-START_ROBOT_CLASS(RobotDemo);
+START_ROBOT_CLASS(RobotDemo)
+;
 
