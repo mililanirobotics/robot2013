c1b3c68233438317df2fbdfda1ac5fca0b82044e
team2853 <pokemonmegman@gmail.com>
2013-01-25 18:02:32 -1000

    Update 2013FinalCode/MyRobot.cpp

--- 2013FinalCode/MyRobot.cpp
+++ 2013FinalCode/MyRobot.cpp
@@ -34,22 +34,29 @@ public:
 				//Potentiometers
 				potFront(1, 1), potBack(1, 2),
 				//Switches
-				switchFront(1,3) , switchFront(1,4){
+				switchFront(1,3) , switchBack(1,4)
+	{
 
 	}
 
 	/**
 	 * Drive left & right motors for 2 seconds then stop
 	 */
-	void Autonomous(void) {
+	bool ClawIsClosed()
+	{
+		return true; //REPLACE THIS BEFORE TESTING
+	}
+	void Autonomous(void) 
+	{
 		//TODO:Autnomous code
 		light.Set(Relay::kOn);
 		bool isAligned;
 		//Pyramid allignment
-		while (!isAligned) {
-			float targety = 0;
+		while (!isAligned) 
+		{
+		//	float targety = 0; UNUSED
 			float targetx = 0;
-			float distance = 0;
+		//	float distance = 0; UNUSED
 			//TODO: Get x and y values from NIVision or RoboRealm
 
 			//This code assumes that center of target is located at 100.
@@ -63,7 +70,8 @@ public:
 				isAligned = true;
 			}
 
-			for (int x = 0; x<3; x++) {  //This code repeats 3 times to shoot all three disks
+			for (int x = 0; x<3; x++) 
+			{  //This code repeats 3 times to shoot all three disks
 				shooterBack.Set(1);
 				shooterFront.Set(1);
 				Wait(1.5); //Gives jags a chance to spin up  (TODO: find actual spin up time)
@@ -86,12 +94,14 @@ public:
 	/**
 	 * Runs the motors with arcade steering. 
 	 */
-	void OperatorControl(void) {
-		DriverStationLCD *screen = DriverStationLCD::GetInstance();
+	void OperatorControl(void) 
+	{
+	//	DriverStationLCD *screen = DriverStationLCD::GetInstance(); UNUSED
 		while (IsOperatorControl()) {
 			light.Set(Relay::kOn);
 			drive.TankDrive(leftStick, rightStick);//tank drive for now, but we can change to fit drivers needs.
-			if (rightStick.GetRawButton(1)) {
+			if (rightStick.GetRawButton(1)) 
+			{
 				shooterBack.Set(1);
 				shooterFront.Set(1);
 				Wait(1.5); //Gives jags a chance to spin up
@@ -108,13 +118,15 @@ public:
 				feederLock.Set(0);
 				//Do we want to put this in a for loop and fire all 4 disks, or shoot individual disks?
 			}
-			if (rightStick.GetRawButton(10)) { //End game code
+			if (rightStick.GetRawButton(10)) //End game code
+			{ 
 				bool isAligned;
 				//Pyramid allignment
-				while (!isAligned) {
-					float targety = 0;
+				while (!isAligned) 
+				{
+				//	float targety = 0; UNUSED
 					float targetx = 0;
-					float distance = 0;
+				//	float distance = 0; UNUSED
 					//TODO: Get x and y values from NIVision or RoboRealm
 					//This code assumes that center of target is located at 100. (TODO: Get actual middle values and replace 100)
 
@@ -127,14 +139,16 @@ public:
 					}
 				}
 				while (potFront.GetVoltage() < (10/3) || switchFront.Get() < 1) { // 10/3 comes from 5(voltage)/270(degrees total)*180 degrees we need to move
-=======
+
 				//TODO: Tell the driver that the robot is aligned
+				}
 			}
 			
 			//TODO: Find out some way to figure out when the claw is closed - possibly a switch?
-			if (grabber.isLocked()){  //Grabber.isLocked is psuedo code - we want to replace this
+			if (ClawIsClosed())
+			{
 				while (potFront.GetVoltage() < (10/3)) { // 10/3 comes from 5(voltage)/270(degrees total)*180 degrees we need to move
->>>>>>> origin/master
+//>>>>>>> origin/master //what the ellhay is this supposed to do???
 					endGameLeftFront.Set(1);
 					endGameRightFront.Set(1);
 				}
@@ -149,9 +163,10 @@ public:
 				//TODO: Repeat this code for the number of times we are going to move the arms
 			}
 		}
+	
+		
 	}
+	
 };
 
-START_ROBOT_CLASS(RobotDemo)
-;
-
+START_ROBOT_CLASS(RobotDemo);
